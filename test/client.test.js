db5a50675a0efdac99e5f5c7cc7a21a814fbd13c
Ethan Smith <ethan@onebytegone.com>
2017-04-14 14:43:15 -0400

    Allow for underscore in username

--- test/client.test.js
+++ test/client.test.js
@@ -112,6 +112,15 @@ describe('Client', function() {
     expect(ret.path).to.equal('/home/admin/path');
   });
 
+  it('can handle "_" in username', function() {
+    ret = client.parse('admin_2:bx%9@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin_2');
+    expect(ret.password).to.equal('bx%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
 });
 
 describe('when calling from windows', function() {
