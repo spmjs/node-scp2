431573de172a350dffb47a9028be91bdca02be0f
Ethan Smith <onebytegone@users.noreply.github.com>
2017-04-17 02:42:27 -0400

    Fix parse unit tests and simplify logic (#90)
    * Fix parse unit tests and simplify the parse logic
    
    In 6fbafab15151e2b756f358ee7e2283b864b7ae88 the regex for parsing was changed
    to add support for ':port'. However, the tests were not updated. This fixes the
    failing tests and simplifies the logic around the parsing.
    
    * Allow for underscore in username

--- test/client.test.js
+++ test/client.test.js
@@ -19,6 +19,64 @@ describe('Client', function() {
     expect(ret.host).to.equal('example.com');
   });
 
+  //  NOTE: At this time, username is always required
+  // it('can parse host and port', function() {
+  //   ret = client.parse('example.com:12345');
+  //   expect(ret.host).to.equal('example.com');
+  //   expect(ret.port).to.equal('12345');
+  // });
+
+  it('can parse username, host, and port', function() {
+    ret = client.parse('admin@example.com:12345');
+    expect(ret.username).to.equal('admin');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+  });
+
+  it('can parse username, password, host, and port', function() {
+    ret = client.parse('admin:bx%9@example.com:12345');
+    expect(ret.username).to.equal('admin');
+    expect(ret.password).to.equal('bx%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+  });
+
+  // NOTE: At this time, username is always required
+  // it('can parse path only', function() {
+  //   ret = client.parse('/home/admin/path');
+  //   expect(ret.path).to.equal('/home/admin/path');
+  // });
+
+  // NOTE: At this time, username is always required
+  // it('can parse host and path', function() {
+  //   ret = client.parse('example.com:/home/admin/path');
+  //   expect(ret.host).to.equal('example.com');
+  //   expect(ret.path).to.equal('/home/admin/path');
+  // });
+
+  // NOTE: At this time, username is always required
+  // it('can parse host, port, and path', function() {
+  //   ret = client.parse('example.com:12345:/home/admin/path');
+  //   expect(ret.host).to.equal('example.com');
+  //   expect(ret.port).to.equal('12345');
+  //   expect(ret.path).to.equal('/home/admin/path');
+  // });
+
+  it('can parse username, host, and path', function() {
+    ret = client.parse('admin@example.com:/home/admin/path');
+    expect(ret.username).to.equal('admin');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
+  it('can parse username, host, port, and path', function() {
+    ret = client.parse('admin@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
   it('can parse username, password, host, and path', function() {
     ret = client.parse('admin:bx%9@example.com:/home/admin/path');
     expect(ret.username).to.equal('admin');
@@ -27,6 +85,41 @@ describe('Client', function() {
     expect(ret.path).to.equal('/home/admin/path');
   });
 
+  it('can parse username, password, host, port, and path', function() {
+    ret = client.parse('admin:bx%9@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin');
+    expect(ret.password).to.equal('bx%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
+  it('can handle "@" in password', function() {
+    ret = client.parse('admin:bx@%9@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin');
+    expect(ret.password).to.equal('bx@%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
+  it('can handle ":" in password', function() {
+    ret = client.parse('admin:bx:%9@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin');
+    expect(ret.password).to.equal('bx:%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
+
+  it('can handle "_" in username', function() {
+    ret = client.parse('admin_2:bx%9@example.com:12345:/home/admin/path');
+    expect(ret.username).to.equal('admin_2');
+    expect(ret.password).to.equal('bx%9');
+    expect(ret.host).to.equal('example.com');
+    expect(ret.port).to.equal('12345');
+    expect(ret.path).to.equal('/home/admin/path');
+  });
 
 });
 
